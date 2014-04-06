28556a31544cd030a8706dce06b5bcafca0cf092
larouxn <larouxn@gmail.com>
2014-04-06 11:58:37 -0400

    Modal has been brought into existence, fully

--- js/Modal.js
+++ js/Modal.js
@@ -1,6 +1,41 @@
 CorduleJS.addModule("Modal", (function(){
-  var displayModal = function(modalOptions){
-    
+  var displayModal = function(tempRegData){
+      var modal = document.getElementById('modal');
+      var modalBackground = document.getElementById('modalBackground');
+      var title = document.getElementById('label');
+      var desc = document.getElementById('description');
+      var save = document.getElementById('saveButton');
+      var del = document.getElementById('deleteButton');
+      var cancel = document.getElementById('cancelButton');
+
+      modal.style.display = "inline";
+      modalBackground.style.display = "inline";
+
+      document.getElementById('label').value = tempRegData.label;
+      document.getElementById('description').value = tempRegData.desc;
+      save.onclick = function() {
+          tempRegData.label = document.getElementById('label').value;
+          tempRegData.desc = document.getElementById('description').value;
+          tempRegData.callback(tempRegData);
+          //save.onclick = "";
+          modal.style.display = "none";
+          modalBackground.style.display = "none";
+      };
+      if(del)
+          del.onclick = function() {
+              tempRegData.regEditMode = 'remove';
+              tempRegData.label = document.getElementById('label').value;
+              tempRegData.desc = document.getElementById('description').value;
+              tempRegData.callback(tempRegData);
+              modal.style.display = "none";
+              modalBackground.style.display = "none";
+          }
+      if(cancel)
+          cancel.onclick = function() {
+              tempRegData.callback(tempRegData);
+              modal.style.display = "none";
+              modalBackground.style.display = "none";
+          }
   }
 
   return {
